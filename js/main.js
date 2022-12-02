816a52256f382cae5a519326b40e51243a5b5abf
Matthew Sirois <mjsirois2@gmail.com>
2022-12-01 20:23:09 -0500

    Declared some JSON, button variables

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 let elements = (json) => {
 
+    let musicSlides = document.querySelector(`.carousel-slides`);
+    let slideContainer = document.createElement(`div`);
+    slideContainer.setAttribute(`class`, `slide-container`);
 
+    for(let i = 0; i <json.length; i++) {
+
+        let albums = `<h2>${json[i].album}</h2>`;
+        slideContainer += albums;
+
+        let artists = `<a>${json[i].artist}</a>`;
+        slideContainer += artists;
+
+        
+
+    }
+
+    musicSlides.innerHTML = slideContainer;
 };
 
 window.onload = () => {
+
+    let arrowButtons = document.getElementsByClassName(`carousel-navigation`);
+    let leftButton = arrowButtons[0];
+    let rightButton = arrowButtons[1];
+
+
     let body = document.querySelector(`body`);
     let script = document.createElement(`script`);
 
     script.setAttribute(`src`, `json/data.json`);
     body.append(script);
-}
+};
